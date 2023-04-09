928b1d46b2e0ac65ae56d4006c18241918f66b95
Hp <adkhamjon.abd@gmail.com>
2023-04-09 13:19:33 +0200

    Corrects mistakes

--- src/main/java/school/mjc/stage0/conditions/task1/NoBracketsPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/NoBracketsPrinter.java
@@ -4,8 +4,9 @@ public class NoBracketsPrinter {
     public static void main(String[] args) {
         if(false){
             System.out.println("The statement is true");
+            System.out.println("The statement is true");
 
         }
-        System.out.println("I'm out of checking");
+
     }
 }
